daa563e6009cc267f7138ce50e0ef1936ad049a8
Calvin <calvinho0302@gmail.com>
2025-04-26 11:46:33 -0700

    Created task router, checking endpoints, and more

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const tasksRouter = require("./tasks/tasks.router.js");
+
+const app = express();
+const port = 3001;
+
+
+app.use("/", tasksRouter);
+
+app.listen(port, ()=>{
+  console.log(`App listening on port no: ${port}`)
+});
