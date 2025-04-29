03603a4dcee20b32331babe5f10db5106035af64
Calvin <calvinho0302@gmail.com>
2025-04-28 20:31:01 -0700

    Adjustments before mongo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+const responseFormatter =  require("./middleware/responseFormatter.js");
+const {StatusCodes} = require("http-status-codes");
+const cors = require("cors");
 const tasksRouter = require("./tasks/tasks.router.js");
+
 
 const app = express();
 const port = 3001;
 
 
+app.use(express.json());
+
+app.use(cors());
+
+
+let accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "..", "access.log"), 
+  {
+  flags: "a",
+}
+);
+
+app.use(morgan("combined", { stream: accessLogStream}));
+app.use(responseFormatter);
+
+//define routes
 app.use("/", tasksRouter);
+
+app.use((req, res) => {
+  res.status(StatusCodes.NOT_FOUND).json(null);
+})
 
 app.listen(port, ()=>{
   console.log(`App listening on port no: ${port}`)
